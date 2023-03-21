7788b9b71647d263087e1bd7ee50ab16b8c0cfd1
Fantasia-Infinity <1134693876@qq.com>
2023-03-21 15:55:13 +0100

    /

--- aot/analysis/video_processing_code/video_reverse.py
+++ aot/analysis/video_processing_code/video_reverse.py
@@ -7,16 +7,19 @@ from pathlib import Path
 import aot
 
 # get reversed video and save it reversed_videos folder for all groups
+
+
 def reverse_video():
     base_dir = Path(aot.__path__[0])
     core_expt_yaml_path = base_dir / "experiment/core_exp_settings.yml"
-    core_settings = yaml.load(open(core_expt_yaml_path), Loader=yaml.FullLoader) 
+    core_settings = yaml.load(
+        open(core_expt_yaml_path), Loader=yaml.FullLoader)
 
-
-    stimuli_processing_path = Path(core_settings["paths"]["stimuli_processing_path"])
-    category_path =  stimuli_processing_path / "raws"
-    streched_path =  stimuli_processing_path / "resampled"
-    reversed_path =  stimuli_processing_path / "reversed"
+    stimuli_processing_path = Path(
+        core_settings["paths"]["stimuli_processing_path"])
+    category_path = stimuli_processing_path / "raws"
+    streched_path = stimuli_processing_path / "resampled"
+    reversed_path = stimuli_processing_path / "reversed"
     # all_group_folders=[folder for folder in os.listdir(group_path) if folder.startswith('group')]
     all_streched_videos = [
         video for video in os.listdir(streched_path) if video.endswith(".mp4")
