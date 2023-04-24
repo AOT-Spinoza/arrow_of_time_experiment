a72b94b6fb8b4075103c81680e3d6255168391b4
Shufan <shufan.z.t@gmail.com>
2023-04-24 21:51:31 +0200

    motion energy

--- aot/analysis/video_analysis/motion_energy/motion_energy.py
+++ aot/analysis/video_analysis/motion_energy/motion_energy.py
@@ -1,23 +1,38 @@
 import moten
 import pickle
+import yaml
+import os
+import aot
+from pathlib import Path
+import aot.analysis.video_processing_code.database_append as db_append
 
-# Stream and convert the RGB video into a sequence of luminance images
-# video_file = '/Users/shufanzhang/Documents/PhD/Arrow_of_time/arrow_of_time_spinoza/stimuli_flat/R_S_-CyI3hUfiOY_23.mp4'
-video_file = (
-    "/tank/shared/2022/arrow_of_time/arrow_of_time4/videos1/R_S__6qAy77ODUI_175.mp4"
-)
-luminance_images = moten.io.video2luminance(video_file)
 
-# Create a pyramid of spatio-temporal gabor filters
-nimages, vdim, hdim = luminance_images.shape
-print(nimages, vdim, hdim)
-pyramid = moten.get_default_pyramid(vhsize=(vdim, hdim), fps=24)
+base_dir = Path(aot.__path__[0])
+core_expt_yaml_path = base_dir / "experiment/core_exp_settings.yml"
+core_settings = yaml.load(open(core_expt_yaml_path, "r"), Loader=yaml.FullLoader)
+stimuli_path = Path(core_settings["paths"]["stimuli_path"])
 
-# Compute motion energy features
-moten_features = pyramid.project_stimulus(luminance_images)
-print(moten_features)
-print(moten_features.shape)
+result_path = base_dir / "analysis/video_analysis/motion_energy/result"
 
-# Save the features
-with open("moten_features.pkl", "wb") as f:
-    pickle.dump(moten_features, f)
+
+def get_motion_energy(video_file):
+    luminance_images = moten.io.video2luminance(video_file)
+
+    # Create a pyramid of spatio-temporal gabor filters
+    nimages, vdim, hdim = luminance_images.shape
+    print(nimages, vdim, hdim)
+    pyramid = moten.get_default_pyramid(vhsize=(vdim, hdim), fps=24)
+
+    # Compute motion energy features
+    moten_features = pyramid.project_stimulus(luminance_images)
+    print(moten_features)
+    print(moten_features.shape)
+
+    return moten_features
+
+
+all_videos = [video for video in os.listdir(stimuli_path) if video.endswith(".mp4")]
+for video in all_videos:
+    print(video)
+    moten_features = get_motion_energy(stimuli_path / video)
+    pickle.dump(moten_features, open(result_path / f"{video}.pkl", "wb"))
