c3e4564789708a3e2f70b0bd81d40d5c1470f381
Fantasia-Infinity <1134693876@qq.com>
2023-08-23 21:35:38 +0200

    /

--- aot/analysis/video_processing_code/gen_pictures.py
+++ aot/analysis/video_processing_code/gen_pictures.py
@@ -15,10 +15,20 @@ def video_to_pic(video_path):
     video_name = video_path.stem
     os.system("ffmpeg -i " + str(video_path) + " -ss 00:00:01.000 -vframes 1 " + str(pictures_path / video_name) + ".png")
 
+
+
 if __name__ == "__main__":
     for video in os.listdir(flat_path):
         if video.endswith(".mp4"):
             video_to_pic(flat_path / video)
+
+    #for files that end with 1080-h264.png, change that port to .png
+    for filename in os.listdir(pictures_path):
+        #add the root path
+        if filename.endswith("1080-h264.png"):
+            filename = pictures_path + "/" + filename
+            print(filename)
+            os.rename(filename, filename[:-13] + "png")
 
 
     
