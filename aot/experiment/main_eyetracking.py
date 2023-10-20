faa59ca15152220a5fb34cfe93d1640e610a1469
Your Name <you@example.com>
2023-10-20 10:44:49 +0200

    eyetraking settings update

--- aot/experiment/main_eyetracking.py
+++ aot/experiment/main_eyetracking.py
@@ -29,7 +29,7 @@ subject, ses, run, eyelink = (
 
 
 def main():
-    settings_dir = base_dir / core_settings["paths"]["settings_path"] / "eyetracking" # shoule not be main anymore
+    settings_dir = base_dir / core_settings["paths"]["settings_path"] / "main"   #"eyetracking" # shoule not be main anymore
     output_dir = base_dir / \
         core_settings["paths"]["output_path"] / "eyetracking"
     output_str = f"sub-{str(subject).zfill(2)}_ses-{str(ses).zfill(2)}_run-{str(run).zfill(2)}_task-movie"
