d262446b74d28ec7199c8ad994c2fb02a4a90775
Your Name <you@example.com>
2023-11-10 13:29:38 +0100

    sub03 session 3 and 4 eyetracking and memory

--- aot/experiment/main_eyetracking.py
+++ aot/experiment/main_eyetracking.py
@@ -12,8 +12,8 @@ core_settings = yaml.load(open(core_expt_yaml_path), Loader=yaml.FullLoader)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--subject", default=3, nargs="?")
-parser.add_argument("--ses", default=3, nargs="?")
-parser.add_argument("--run", default=1, nargs="?")
+parser.add_argument("--ses", default=4, nargs="?")
+parser.add_argument("--run", default=10, nargs="?")
 
 parser.add_argument("--eyelink", default=True,
                     action=argparse.BooleanOptionalAction)
