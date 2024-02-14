9f53726b3a7cdc1bf8a3397af0345df10150711c
Your Name <you@example.com>
2024-02-14 13:05:25 +0100

    sub5 ses5 and ses6 till run 6

--- aot/experiment/main_eyetracking.py
+++ aot/experiment/main_eyetracking.py
@@ -12,8 +12,8 @@ core_settings = yaml.load(open(core_expt_yaml_path), Loader=yaml.FullLoader)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--subject", default=5, nargs="?")
-parser.add_argument("--ses", default=5, nargs="?")
-parser.add_argument("--run", default=1, nargs="?")
+parser.add_argument("--ses", default=6, nargs="?")
+parser.add_argument("--run", default=6, nargs="?")
 parser.add_argument("--eyelink", default=True,
                     action=argparse.BooleanOptionalAction)
 
